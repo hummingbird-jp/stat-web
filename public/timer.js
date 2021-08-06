4ab5f5068975459fff79e24414cac4fc44ee6952
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-08-06 19:01:22 +0900

    refactor(timer.js): shorten codes
    replace almost every query statements to jQuery

--- public/timer.js
+++ public/timer.js
@@ -1,92 +1,88 @@
-var lockObj = false;
+const sliderInputElem = $("#timer-duration")[0];
+let lockObj = false;
+
+$(sliderInputElem).on("input", () => setCurrentValue(e.target.value));
+setCurrentValue(sliderInputElem.value);
 
 function getTimeRemaining(endtime) {
-    const total = Date.parse(endtime) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+	const total = Date.parse(endtime) - Date.parse(new Date());
 
-    return {
-        total,
-        days,
-        hours,
-        minutes,
-        seconds
-    };
+	const seconds = Math.floor((total / 1000) % 60);
+	const minutes = Math.floor((total / 1000 / 60) % 60);
+	const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+	const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+	return {
+		total,
+		days,
+		hours,
+		minutes,
+		seconds
+	};
 }
 
 function initializeClock(id, endtime) {
-    const clock = document.getElementById(id);
+	const clock = document.getElementById(id);
 
-    const minutesSpan = clock.querySelector('.minutes');
-    const secondsSpan = clock.querySelector('.seconds');
+	const minutesSpan = clock.querySelector('.minutes');
+	const secondsSpan = clock.querySelector('.seconds');
 
-    clock.classList.remove("flashTimer");
+	clock.classList.remove("flashTimer");
 
-    function updateClock() {
-        if (lockObj == true) {
-            clearInterval(timeinterval);
-            lockObj = false;
-            return 0;
-        }
+	function updateClock() {
+		if (lockObj === true) {
+			clearInterval(timeinterval);
+			lockObj = false;
+			return 0;
+		}
 
-        const t = getTimeRemaining(endtime);
+		const t = getTimeRemaining(endtime);
 
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		$(minutesSpan).text(('0' + t.minutes).slice(-2));
+		$(secondsSpan).text(('0' + t.seconds).slice(-2));
 
-        if (t.total <= 0) {
-            clearInterval(timeinterval);
-            clock.classList.add("flashTimer");
-        }
-    }
+		if (t.total <= 0) {
+			clearInterval(timeinterval);
+			clock.classList.add("flashTimer");
+		}
+	}
 
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
+	updateClock();
+	const timeinterval = setInterval(updateClock, 1000);
 }
 
 function getDeadline(val) {
-    const deadline = new Date(Date.parse(new Date()) + val * 60 * 1000);
-    return deadline;
+	return new Date(Date.parse(new Date()) + val * 60 * 1000);
 }
 
 function startTimer() {
-    var duration = document.getElementById("timer-duration").value;
-    var deadline = getDeadline(duration);
-    initializeClock('clockdiv', deadline);
-    document.getElementById("timer-slider").style.visibility = "hidden";
-    document.getElementById("start-timer").style.display = "none";
-    document.getElementById("stop-timer").style.display = "inline";
+	const duration = $("#timer-duration").val();
+	const deadline = getDeadline(duration);
+
+	initializeClock('clockdiv', deadline);
+
+	$("#timer-slider").css("visibility", "hidden");
+	$("#start-timer").css("display", "none");
+	$("#stop-timer").css("display", "inline");
 }
 
 function stopTimer() {
-    lockObj = true;
-    document.getElementById("timer-slider").style.visibility = "visible";
-    document.getElementById("stop-timer").style.display = "none";
-    document.getElementById("start-timer").style.display = "inline";
-}
+	lockObj = true;
 
-// slider functions
-const sliderInputElem = document.getElementById("timer-duration");
+	$("#timer-slider").css("visibility", "visible");
+	$("#stop-timer").css("display", "none");
+	$("#start-timer").css("display", "inline");
+}
 
 function setCurrentValue(val) {
-    const clock = document.getElementById("clockdiv");
+	const clock = $("#clockdiv")[0];
 
-    const minutesSpan = clock.querySelector('.minutes');
-    const secondsSpan = clock.querySelector('.seconds');
+	const minutesSpan = clock.querySelector('.minutes');
+	const secondsSpan = clock.querySelector('.seconds');
 
-    const endtime = getDeadline(val);
-    const t = getTimeRemaining(endtime);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-}
+	const endtime = getDeadline(val);
+	const t = getTimeRemaining(endtime);
 
-function rangeOnChange(e) {
-    setCurrentValue(e.target.value);
-}
-
-window.onload = () => {
-    sliderInputElem.addEventListener('input', rangeOnChange);
-    setCurrentValue(sliderInputElem.value);
+	$(minutesSpan).text(('0' + t.minutes).slice(-2));
+	$(secondsSpan).text(('0' + t.seconds).slice(-2));
 }
