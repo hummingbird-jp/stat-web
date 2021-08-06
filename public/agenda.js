10db7b8efad819cdeca48afa3ec7da1823094b8c
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-08-06 19:24:38 +0900

    chore(agenda.js): change file name: set-agenda.js -> agenda.js

---
+++ public/agenda.js
@@ -0,0 +1,5 @@
+function showValue() {
+	const agenda = $("#agenda-in").val();
+
+	$("#agenda-out").text(agenda);
+}
--- public/set-agenda.js
+++
@@ -1,9 +0,0 @@
-// function showDiv() {
-//     document.getElementById('hello').style.display = "block";
-// }
-
-function showValue() {
-    var agenda = document.getElementById('agenda-in').value;
-    document.getElementById('agenda-out').innerHTML = agenda;
-    // document.getElementById('hello').style.display = "none";
-}
