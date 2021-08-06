d3faf1e57443c6ae505993b5a863bb79214d096a
須田隆太朗 <rsuda@RS-MacBook-Air.local>
2021-08-06 13:52:47 +0900

    feat(timer.js & set-agenda.js): add basic timer and agenda functions

---
+++ public/set-agenda.js
@@ -0,0 +1,9 @@
+// function showDiv() {
+//     document.getElementById('hello').style.display = "block";
+// }
+
+function showValue() {
+    var agenda = document.getElementById('agenda-in').value;
+    document.getElementById('agenda-out').innerHTML = agenda;
+    // document.getElementById('hello').style.display = "none";
+}
