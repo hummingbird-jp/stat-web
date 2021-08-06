94bb70bf69d62ee336cd2169f5b714acad11fe2e
須田隆太朗 <rsuda@RS-MacBook-Air.local>
2021-08-06 17:28:06 +0900

    feat(timer.js): add slidebar to select duration of the timer.

--- public/timer.js
+++ public/timer.js
@@ -1,3 +1,5 @@
+var lockObj = false;
+
 function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
     const seconds = Math.floor((total / 1000) % 60);
@@ -16,21 +18,27 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
     const clock = document.getElementById(id);
-    // const daysSpan = clock.querySelector('.days');
-    // const hoursSpan = clock.querySelector('.hours');
+
     const minutesSpan = clock.querySelector('.minutes');
     const secondsSpan = clock.querySelector('.seconds');
 
+    clock.classList.remove("flashTimer");
+
     function updateClock() {
+        if (lockObj == true) {
+            clearInterval(timeinterval);
+            lockObj = false;
+            return 0;
+        }
+
         const t = getTimeRemaining(endtime);
 
-        // daysSpan.innerHTML = t.days;
-        // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
         if (t.total <= 0) {
             clearInterval(timeinterval);
+            clock.classList.add("flashTimer");
         }
     }
 
@@ -38,10 +46,47 @@ function initializeClock(id, endtime) {
     const timeinterval = setInterval(updateClock, 1000);
 }
 
-function resetClock(){
-    const deadline = new Date(Date.parse(new Date()) + 10 * 60 * 1000);
-    initializeClock('clockdiv', deadline);
+function getDeadline(val) {
+    const deadline = new Date(Date.parse(new Date()) + val * 60 * 1000);
+    return deadline;
 }
 
-const deadline = new Date(Date.parse(new Date()) + 10 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+function startTimer() {
+    var duration = document.getElementById("timer-duration").value;
+    var deadline = getDeadline(duration);
+    initializeClock('clockdiv', deadline);
+    document.getElementById("timer-slider").style.visibility = "hidden";
+    document.getElementById("start-timer").style.display = "none";
+    document.getElementById("stop-timer").style.display = "inline";
+}
+
+function stopTimer() {
+    lockObj = true;
+    document.getElementById("timer-slider").style.visibility = "visible";
+    document.getElementById("stop-timer").style.display = "none";
+    document.getElementById("start-timer").style.display = "inline";
+}
+
+// slider functions
+const sliderInputElem = document.getElementById("timer-duration");
+
+function setCurrentValue(val) {
+    const clock = document.getElementById("clockdiv");
+
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+
+    const endtime = getDeadline(val);
+    const t = getTimeRemaining(endtime);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+}
+
+function rangeOnChange(e) {
+    setCurrentValue(e.target.value);
+}
+
+window.onload = () => {
+    sliderInputElem.addEventListener('input', rangeOnChange);
+    setCurrentValue(sliderInputElem.value);
+}
