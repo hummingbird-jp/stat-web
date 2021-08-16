4f8ddfef68c806974034876b0b7e0639d07cd628
須田隆太朗 <rsuda@RS-MacBook-Air.local>
2021-08-16 19:35:58 +0900

    feat(bgm.js): Now we can share BGM with others via Firestore

--- public/bgm.js
+++ public/bgm.js
@@ -1,78 +1,52 @@
-const audioSet = [{
-        uri: "https://firebasestorage.googleapis.com/v0/b/stat-web-6372a.appspot.com/o/bgm%2Fnature_sound.mp3?alt=media&token=aff3df2f-787d-43e6-be5f-2a51cae2abef",
-        category: "nature",
-        caption: "Be natural"
-    },
-    {
-        uri: "https://firebasestorage.googleapis.com/v0/b/stat-web-6372a.appspot.com/o/bgm%2FAfternoon%20Lounge%20Jazz%20-%20Relaxing%20Jazz%20Music%20for%20Work%20%26%20Study%20-%20from%20YouTube.mp3?alt=media&token=a13dec52-484e-46ab-bc00-e10f8a9d5f14",
-        category: "jazz",
-        caption: "Be relaxed"
-    },
-    {
-        uri: "https://firebasestorage.googleapis.com/v0/b/stat-web-6372a.appspot.com/o/bgm%2F%E2%98%95%20Coffee%20Shop%20Noise%20for%20Studying%20_%20Pomodoro%20Technique%202%20Hours%20Timer%20-%20from%20YouTube.mp3?alt=media&token=c277349d-2c6d-47b5-b62f-241a561984d1",
-        category: "buzz",
-        caption: "Be focused"
-    }
-];
-
-// Play bgm automatically when loaded.
-const uriAudioDefault = audioSet[0].uri;
+/*
+ * Need to allocate uri to Audio element when created.
+ * So one of the BGMs is allocated as default, but it is not played.
+ */
+const uriAudioDefault = "https://firebasestorage.googleapis.com/v0/b/stat-web-6372a.appspot.com/o/bgm%2Fnature_sound.mp3?alt=media&token=aff3df2f-787d-43e6-be5f-2a51cae2abef";
 
 var audioElm = new Audio(uriAudioDefault);
-configureDefaultAudio(audioElm);
+configureAudioDefault(audioElm);
 
-const selectorObj = document.getElementById("bgm-selector");
-const playButton = document.getElementById("play-button");
-const stopButton = document.getElementById("stop-button");
-const playbackSpan = document.getElementById("playback-span");
+const selectorObj = $("#bgm-selector");
+const playButton = $("#play-button");
+const stopButton = $("#stop-button");
+const playbackSpan = $("#playback-span");
 
-// play/pause audio
+/*
+ * Firestore related constants
+ * TODO: syncBgmDocId is beta, currently a document id is given. 
+ */
+var db = firebase.firestore();
+const syncBgmDocId = "tQ4vN4gwalYtkbTI8Py3";
+const syncBgmCollection = "sync-bgm-beta";
+const audioSetCollection = "audioSet";
+
+/*
+ * Stop BGM when left.
+ */
+$("#leave").click(function(e) {
+    audioElm.pause();
+});
+
+/*
+ * Send BGM status to database (Firestore)
+ */
 playButton.addEventListener('click', function() {
+
+    var currentTime = audioElm.currentTime;
 
     if (this.dataset.playing === "preSelect") {
         if (selectorObj.value === "default") {
             console.log("BGM not selected.");
         } else {
-            // audioElm.src = uriAudio;
-            // audioElm.pause()
-            // audioElm.load();
-            // configureDefaultAudio(audioElm);
-            // audioElm.play();
-            // this.dataset.playing = "true";
-            // configureControlPanelPlaying();
-
-            // var category = selectorObj.value;
-            // console.log("category: ", category);
-            // db.collection("audioSet").where("category", "==", category)
-            //     .limit(1)
-            //     .get()
-            //     .then((querySnapshot) => {
-            //         querySnapshot.forEach((doc) => {
-            //             console.log(doc.id, " => ", doc.data());
-            //             var currentTrackId = doc.id;
-            //             db.collection("sync-bgm-beta").doc(syncBgmDocId).set({
-            //                 currentTime: 0,
-            //                 currentTrackId: currentTrackId,
-            //                 isChanged: true,
-            //                 isPlaying: true
-            //             })
-            //         });
-            //     })
-            //     .catch((error) => {
-            //         console.log("Error getting documents: ", error);
-            //     });
             sendBgmStatus(0, true, true);
         }
     } else if (this.dataset.playing === 'false') { // when "Play" clicked
-        var currentTime = audioElm.currentTime;
         sendBgmStatus(currentTime, false, true);
-        // audioElm.play();
-        // configureControlPanelPlaying();
+
     } else if (this.dataset.playing === 'true') { // when "Pause" clicked
-        var currentTime = audioElm.currentTime;
         sendBgmStatus(currentTime, false, false);
-        // audioElm.pause();
-        // configureControlPanelPaused();
+
     }
 
     let state = this.getAttribute('aria-checked') === "true" ? true : false;
@@ -80,17 +54,72 @@ playButton.addEventListener('click', function() {
 
 }, false);
 
+stopButton.addEventListener("click", function() {
+    configureControlPanelDefault();
+});
+
+db.collection(syncBgmCollection).doc(syncBgmDocId) // listen to "currentTrackId"
+    .onSnapshot((doc) => {
+
+        // Only for debugging
+        console.log("Current data: ", doc.data());
+
+        var currentTrackId = doc.data().currentTrackId;
+        var currentTime = doc.data().currentTime;
+        var isPlaying = doc.data().isPlaying;
+        var isChanged = doc.data().isChanged;
+
+        if (!$(".meeting-area").is(":hidden")) {
+
+            if (isChanged) {
+
+                var docRef = db.collection(audioSetCollection).doc(currentTrackId);
+
+                docRef.get().then((doc) => {
+                    if (doc.exists) {
+
+                        // Only for debugging
+                        console.log("audioSet data: ", doc.data());
+
+                        changeTrackTo(doc.data().uri, currentTime);
+                        changeSelectorTo(doc.data().category);
+                        configureControlPanelPlaying();
+                    }
+                })
+            } else {
+                /*
+                 * If audio track is not changed but paused or resumed
+                 */
+                if (isPlaying) {
+                    audioElm.currentTime = currentTime;
+                    audioElm.play()
+                    configureControlPanelPlaying();
+                } else {
+                    audioElm.pause();
+                    audioElm.currentTime = currentTime;
+                    configureControlPanelPaused();
+                }
+            }
+        }
+    });
+
 function sendBgmStatus(currentTime, isChanged, isPlaying) {
     var category = selectorObj.value;
+
+    // Only for debugging
     console.log("category: ", category);
-    db.collection("audioSet").where("category", "==", category)
+
+    db.collection(audioSetCollection).where("category", "==", category)
         .limit(1)
         .get()
         .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
+
+                // Only for debugging
                 console.log(doc.id, " => ", doc.data());
+
                 var currentTrackId = doc.id;
-                db.collection("sync-bgm-beta").doc(syncBgmDocId).set({
+                db.collection(syncBgmCollection).doc(syncBgmDocId).set({
                     currentTime: currentTime,
                     currentTrackId: currentTrackId,
                     isChanged: isChanged,
@@ -103,21 +132,14 @@ function sendBgmStatus(currentTime, isChanged, isPlaying) {
         });
 }
 
-stopButton.addEventListener("click", function() {
-    sendBgmStatus(0, false, false);
-    // audioElm.pause();
-    // audioElm.currentTime = 0;
-    configureDefaultControlPanel();
-});
-
-function configureDefaultAudio(audioElm) {
+function configureAudioDefault(audioElm) {
     audioElm.preload = 'none';
     audioElm.loop = true;
     audioElm.autoplay = false;
     audioElm.volume = 0.05;
 }
 
-function configureDefaultControlPanel() {
+function configureControlPanelDefault() {
     stopButton.disabled = true;
     selectorObj.disabled = false;
     selectorObj.value = "default";
@@ -128,7 +150,7 @@ function configureDefaultControlPanel() {
 function configureControlPanelPlaying() {
     playbackSpan.textContent = "Pause";
     playButton.dataset.playing = 'true';
-    stopButton.disabled = false;
+    stopButton.disabled = true;
     selectorObj.disabled = true;
 }
 
@@ -136,5 +158,19 @@ function configureControlPanelPaused() {
     playbackSpan.textContent = "Play";
     playButton.dataset.playing = 'false';
     stopButton.disabled = false;
+    selectorObj.disabled = true;
+}
+
+function changeTrackTo(uri, currentTime) {
+    audioElm.src = uri;
+    audioElm.pause();
+    audioElm.load();
+    configureAudioDefault(audioElm);
+    audioElm.currentTime = currentTime;
+    audioElm.play();
+}
+
+function changeSelectorTo(value) {
+    selectorObj.value = value;
     selectorObj.disabled = true;
 }
