168da96afd6aa7d27c8add3e869dd96d9e873143
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-08-19 22:45:45 +0900

    feat(agenda.js): we can now send agenda to Firestore!
    just sending; cannot listen for now

--- public/agenda.js
+++ public/agenda.js
@@ -1,3 +1,25 @@
-function showValue() {
-	$("#agenda-out").text($("#agenda-in").val());
+const setAgendaButton = $('#set-agenda')[0];
+
+$(setAgendaButton).click(function (e) {
+	const agenda = $("#agenda-in").val();
+
+	$("#agenda-out").text(agenda);
+	sendAgenda(agenda);
+});
+
+// Send agenda to Firestore > sync-agenda-beta collection
+function sendAgenda(agenda) {
+	const syncAgendaDocId = "m3xucfEwuennPStNWq0M";
+	const syncAgendaCollection = "sync-agenda-beta";
+	const agendaRef = db.collection(syncAgendaCollection).doc(syncAgendaDocId);
+
+	console.log(`Sending agenda: ${agenda}`);
+
+	agendaRef.set({
+		agenda: agenda,
+	}).then(() => {
+		console.log(`Agenda successfully sent!`);
+	}).catch((err) => {
+		console.error(`Error sending agenda: ${err}`);
+	});
 }
