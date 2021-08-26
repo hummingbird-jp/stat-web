259aeede13ff17761fcc80720fbca9143f916987
須田隆太朗 <rsuda@RS-MacBook-Air.local>
2021-08-26 19:31:16 +0900

    Revert "feat(index.html,bgm.js): we can change bgm volume using slider"
    This reverts commit 48a0cbab8f6f44c62234a781569f8b1d30c2c4b4.

--- public/bgm.js
+++ public/bgm.js
@@ -11,7 +11,6 @@ const selectorObj = $("#bgm-selector")[0];
 const playButton = $("#play-button")[0];
 const stopButton = $("#stop-button")[0];
 const playbackSpan = $("#playback-span")[0];
-const volumeSlider = $("#bgm-volume")[0];
 
 const syncBgmCollection = "sync-bgm-beta";
 const audioSetCollection = "audioSet";
@@ -19,164 +18,155 @@ const audioSetCollection = "audioSet";
 /*
  * Stop BGM when left.
  */
-$("#leave").click(function(e) {
-    audioElm.pause();
+$("#leave").click(function (e) {
+	audioElm.pause();
 });
 
 /*
  * Send BGM status to database (Firestore)
  */
-playButton.addEventListener('click', function() {
+playButton.addEventListener('click', function () {
 
-    const currentTime = audioElm.currentTime;
+	const currentTime = audioElm.currentTime;
 
-    if (this.dataset.playing === "preSelect") {
-        if (selectorObj.value === "default") {
-            console.log("BGM not selected.");
-        } else {
-            sendBgmStatus(0, true, true);
-        }
-    } else if (this.dataset.playing === 'false') { // when "Play" clicked
-        sendBgmStatus(currentTime, false, true);
+	if (this.dataset.playing === "preSelect") {
+		if (selectorObj.value === "default") {
+			console.log("BGM not selected.");
+		} else {
+			sendBgmStatus(0, true, true);
+		}
+	} else if (this.dataset.playing === 'false') { // when "Play" clicked
+		sendBgmStatus(currentTime, false, true);
 
-    } else if (this.dataset.playing === 'true') { // when "Pause" clicked
-        sendBgmStatus(currentTime, false, false);
+	} else if (this.dataset.playing === 'true') { // when "Pause" clicked
+		sendBgmStatus(currentTime, false, false);
 
-    }
+	}
 
-    let state = this.getAttribute('aria-checked') === "true" ? true : false;
-    this.setAttribute('aria-checked', state ? "false" : "true");
+	let state = this.getAttribute('aria-checked') === "true" ? true : false;
+	this.setAttribute('aria-checked', state ? "false" : "true");
 
 }, false);
 
-stopButton.addEventListener("click", function() {
-    configureControlPanelDefault();
+stopButton.addEventListener("click", function () {
+	configureControlPanelDefault();
 });
 
-/*
- * Change local volume (BGM only)
- */
-$(volumeSlider).on("input", (e) => setAudioVolume(e.target.value));
-
 function listenBgm() {
-    db.collection(syncBgmCollection).doc(meetingId) // listen to "currentTrackId"
-        .onSnapshot((doc) => {
+	db.collection(syncBgmCollection).doc(meetingId) // listen to "currentTrackId"
+		.onSnapshot((doc) => {
 
-            // Only for debugging
-            // console.log("Current data: ", doc.data());
+			// Only for debugging
+			// console.log("Current data: ", doc.data());
 
-            const currentTrackId = doc.data().currentTrackId;
-            const currentTime = doc.data().currentTime;
-            const isPlaying = doc.data().isPlaying;
-            const isChanged = doc.data().isChanged;
+			const currentTrackId = doc.data().currentTrackId;
+			const currentTime = doc.data().currentTime;
+			const isPlaying = doc.data().isPlaying;
+			const isChanged = doc.data().isChanged;
 
-            if (!$(".meeting-area").is(":hidden")) {
+			if (!$(".meeting-area").is(":hidden")) {
 
-                if (isChanged) {
+				if (isChanged) {
 
-                    const docRef = db.collection(audioSetCollection).doc(currentTrackId);
+					const docRef = db.collection(audioSetCollection).doc(currentTrackId);
 
-                    docRef.get().then((doc) => {
-                        if (doc.exists) {
+					docRef.get().then((doc) => {
+						if (doc.exists) {
 
-                            // Only for debugging
-                            // console.log("audioSet data: ", doc.data());
+							// Only for debugging
+							// console.log("audioSet data: ", doc.data());
 
-                            changeTrackTo(doc.data().uri, currentTime);
-                            changeSelectorTo(doc.data().category);
-                            configureControlPanelPlaying();
-                        }
-                    })
-                } else {
-                    /*
-                     * If audio track is not changed but paused or resumed
-                     */
-                    if (isPlaying) {
-                        audioElm.currentTime = currentTime;
-                        audioElm.play()
-                        configureControlPanelPlaying();
-                    } else {
-                        audioElm.pause();
-                        audioElm.currentTime = currentTime;
-                        configureControlPanelPaused();
-                    }
-                }
-            }
-        });
+							changeTrackTo(doc.data().uri, currentTime);
+							changeSelectorTo(doc.data().category);
+							configureControlPanelPlaying();
+						}
+					})
+				} else {
+					/*
+					 * If audio track is not changed but paused or resumed
+					 */
+					if (isPlaying) {
+						audioElm.currentTime = currentTime;
+						audioElm.play()
+						configureControlPanelPlaying();
+					} else {
+						audioElm.pause();
+						audioElm.currentTime = currentTime;
+						configureControlPanelPaused();
+					}
+				}
+			}
+		});
 }
 
 function sendBgmStatus(currentTime, isChanged, isPlaying) {
-    const category = selectorObj.value;
+	const category = selectorObj.value;
 
-    // Only for debugging
-    // console.log("category: ", category);
+	// Only for debugging
+	// console.log("category: ", category);
 
-    db.collection(audioSetCollection).where("category", "==", category)
-        .limit(1)
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
+	db.collection(audioSetCollection).where("category", "==", category)
+		.limit(1)
+		.get()
+		.then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
 
-                // Only for debugging
-                // console.log(doc.id, " => ", doc.data());
+				// Only for debugging
+				// console.log(doc.id, " => ", doc.data());
 
-                const currentTrackId = doc.id;
-                db.collection(syncBgmCollection).doc(meetingId).set({
-                    currentTime: currentTime,
-                    currentTrackId: currentTrackId,
-                    isChanged: isChanged,
-                    isPlaying: isPlaying
-                });
-            });
-        })
-        .catch((error) => {
-            console.log("Error getting documents: ", error);
-        });
+				const currentTrackId = doc.id;
+				db.collection(syncBgmCollection).doc(meetingId).set({
+					currentTime: currentTime,
+					currentTrackId: currentTrackId,
+					isChanged: isChanged,
+					isPlaying: isPlaying
+				});
+			});
+		})
+		.catch((error) => {
+			console.log("Error getting documents: ", error);
+		});
 }
 
 function configureAudioDefault(audioElm) {
-    audioElm.preload = 'none';
-    audioElm.loop = true;
-    audioElm.autoplay = false;
-    audioElm.volume = 0.05;
+	audioElm.preload = 'none';
+	audioElm.loop = true;
+	audioElm.autoplay = false;
+	audioElm.volume = 0.05;
 }
 
 function configureControlPanelDefault() {
-    stopButton.disabled = true;
-    selectorObj.disabled = false;
-    selectorObj.value = "default";
-    playbackSpan.textContent = "Play";
-    playButton.dataset.playing = "preSelect";
+	stopButton.disabled = true;
+	selectorObj.disabled = false;
+	selectorObj.value = "default";
+	playbackSpan.textContent = "Play";
+	playButton.dataset.playing = "preSelect";
 }
 
 function configureControlPanelPlaying() {
-    playbackSpan.textContent = "Pause";
-    playButton.dataset.playing = 'true';
-    stopButton.disabled = true;
-    selectorObj.disabled = true;
+	playbackSpan.textContent = "Pause";
+	playButton.dataset.playing = 'true';
+	stopButton.disabled = true;
+	selectorObj.disabled = true;
 }
 
 function configureControlPanelPaused() {
-    playbackSpan.textContent = "Play";
-    playButton.dataset.playing = 'false';
-    stopButton.disabled = false;
-    selectorObj.disabled = true;
+	playbackSpan.textContent = "Play";
+	playButton.dataset.playing = 'false';
+	stopButton.disabled = false;
+	selectorObj.disabled = true;
 }
 
 function changeTrackTo(uri, currentTime) {
-    audioElm.src = uri;
-    audioElm.pause();
-    audioElm.load();
-    configureAudioDefault(audioElm);
-    audioElm.currentTime = currentTime;
-    audioElm.play();
+	audioElm.src = uri;
+	audioElm.pause();
+	audioElm.load();
+	configureAudioDefault(audioElm);
+	audioElm.currentTime = currentTime;
+	audioElm.play();
 }
 
 function changeSelectorTo(value) {
-    selectorObj.value = value;
-    selectorObj.disabled = true;
-}
-
-function setAudioVolume(value) {
-    audioElm.volume = value;
+	selectorObj.value = value;
+	selectorObj.disabled = true;
 }
