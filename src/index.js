a351e89c6a9147e48ca0f830de92b859b4480221
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-09-16 08:58:06 +0900

    feat(sign-in-with-google): add a button to sign in

--- src/index.js
+++ src/index.js
@@ -25,15 +25,16 @@ let meetingId;
 initScreen();
 initAgora();
 
-$(() => {
+$("#sign-in-with-google").on("click", function () {
+	stat_auth.signin();
+	// TODO: show error screen if log in failed
+	$("#sign-in-with-google").hide();
+
 	var urlParams = new URL(location.href).searchParams;
 
 	options.token = urlParams.get("token");
 	options.channel = urlParams.get("channel");
 
-	stat_auth.signin();
-
-	// TODO: show error screen if log in failed
 
 	if (options.token && options.channel) {
 		options.token = options.token.replaceAll(' ', '+');
